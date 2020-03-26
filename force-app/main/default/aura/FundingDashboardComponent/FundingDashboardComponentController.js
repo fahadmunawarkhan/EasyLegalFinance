441eab920a8d008acf20d5e0c79ed1cd67ab3824
UsamaKhalid024 <usamakhalid743@gmail.com>
2020-03-26 17:31:43 +0500

    Update FundingDashboard Component

--- force-app/main/default/aura/FundingDashboardComponent/FundingDashboardComponentController.js
+++ force-app/main/default/aura/FundingDashboardComponent/FundingDashboardComponentController.js
@@ -1,127 +1,125 @@
 ({
-    doInit : function(component, event, helper) {
-        
+    doInit: function(component, event, helper) {
+
         component.set("v.spinner", true);
-        
+
         var options = [
             { value: "ELFI", label: "ELFI" },
             { value: "Rhino", label: "Rhino" },
             { value: "Consolidated", label: "Consolidated" }
         ];
         component.set("v.businessUnitOptions", options);
-        
+
         helper.getCalendarMin(component);
         helper.getCalendarMax(component);
+        helper.getPickListValues(component, 'Opportunity', 'Type_of_Loan__c', 'typeOfLoanOptions');
 
         helper.getCustomSettings(component).then($A.getCallback(
-            function(result){ 
+            function(result) {
                 component.set('v.customSetting', result);
                 return helper.setDefaultDates(component);
-                
-            })).then($A.getCallback(function(result){
+
+            })).then($A.getCallback(function(result) {
             return helper.getData(component);
-        })).then($A.getCallback(function(result){
-            
-            component.set("v.spinner", false);  
+        })).then($A.getCallback(function(result) {
+
+            component.set("v.spinner", false);
             component.set('v.mainData', result);
-            
-                console.log('Allamount map has got');
-                console.log(component.get('v.mainData.numActiveFiles'));
+
+            console.log('Allamount map has got');
+            console.log(component.get('v.mainData.numActiveFiles'));
 
         })).catch(
-            function(errors){
+            function(errors) {
                 console.log(errors);
                 component.set("v.spinner", false);
                 helper.errorsHandler(errors);
-                component.set("v.spinner", false);  
+                component.set("v.spinner", false);
             }
         );
-        
-              
+
+
     },
-    
-    filterButton : function(component, event, helper){
-        
+
+    filterButton: function(component, event, helper) {
+
         component.set("v.spinner", true);
-        
+
         helper.setBUCustomSettings(component);
-        
+
         helper.getData(component).then($A.getCallback(
-            function(result){
+            function(result) {
                 helper.setDateCustomSettings(component);
                 component.set('v.mainData', result);
                 component.set('v.showDetailsCard', component.get('v.showDetails'));
             }
         )).then(
-            function(){
-                
+            function() {
+
                 component.set("v.spinner", false);
             }
         ).catch(
-            function(errors){
+            function(errors) {
                 component.set("v.spinner", false);
                 helper.errorsHandler(errors);
             }
         );
     },
-    sort: function(component, event, helper) {  
-        
+    sort: function(component, event, helper) {
+
         let selectedItem = event.currentTarget;
         let field = selectedItem.dataset.field;
         let sortOrder = component.get('v.sortOrder');
         let oldField = component.get('v.sortField');
-        
+
         sortOrder = (sortOrder == 'DESC' && oldField == field) ? 'ASC' : 'DESC';
-        
-        component.set('v.sortField',field);   
-        component.set('v.sortOrder',sortOrder); https://easylegalfinance--qa.cs24.my.salesforce.com/_ui/common/apex/debug/ApexCSIPage#
-        
-        component.set("v.spinner", true);
-        
+
+        component.set('v.sortField', field);
+        component.set('v.sortOrder', sortOrder);
+        https: //easylegalfinance--qa.cs24.my.salesforce.com/_ui/common/apex/debug/ApexCSIPage#
+
+            component.set("v.spinner", true);
+
         //helper.getLawyersList(component,event);
         helper.getData(component).then($A.getCallback(
-            function(result){ 
+            function(result) {
                 component.set('v.mainData', result);
-                
+
                 console.log('Allamount map has got');
                 console.log(result);
-                
+
                 return helper.GetFileTotalAndAmountTotal(component);
-            })
-                                                     ).then($A.getCallback(
-            function(result){
-                
+            })).then($A.getCallback(
+            function(result) {
+
                 component.set("v.spinner", false);
-            })
-                                                           ).catch(
-            function(errors){
+            })).catch(
+            function(errors) {
                 component.set("v.spinner", false);
                 helper.errorsHandler(errors);
             }
-        );         
+        );
     },
-    viewAllButton : function (component, event, helper) {
-		        
+    viewAllButton: function(component, event, helper) {
+
         let newWin;
-        let url = '/apex/FundingDashboardViewAll?StartDate='+component.get('v.startDate')+'&EndDate='+component.get('v.endDate')+'&BusinessUnit='+component.get('v.selectedBusinessUnitFilter');
-        try{                       
+        let url = '/apex/FundingDashboardViewAll?StartDate=' + component.get('v.startDate') + '&EndDate=' + component.get('v.endDate') + '&BusinessUnit=' + component.get('v.selectedBusinessUnitFilter');
+        try {
             newWin = window.open(url);
-        }catch(e){}
-        if(!newWin || newWin.closed || typeof newWin.closed=='undefined')
-        {
-            reject([{message: 'Pop-up is blocked please click allow in the top right corner of browser in address bar!'}]);
+        } catch (e) {}
+        if (!newWin || newWin.closed || typeof newWin.closed == 'undefined') {
+            reject([{ message: 'Pop-up is blocked please click allow in the top right corner of browser in address bar!' }]);
         }
     },
-    printButton : function (component, event, helper) {
-		        
+    printButton: function(component, event, helper) {
+
         let newWin;
-        let url = '/apex/FundingDashboardPrintReport?StartDate='+component.get('v.startDate')+'&EndDate='+component.get('v.endDate')+'&BusinessUnit='+component.get('v.selectedBusinessUnitFilter');
-        try{                       
+        let url = '/apex/FundingDashboardPrintReport?StartDate=' + component.get('v.startDate') + '&EndDate=' + component.get('v.endDate') + '&BusinessUnit=' + component.get('v.selectedBusinessUnitFilter');
+        try {
             newWin = window.open(url);
-        }catch(e){}
-        if(!newWin || newWin.closed || typeof newWin.closed=='undefined')
-        {
-            reject([{message: 'Pop-up is blocked please click allow in the top right corner of browser in address bar!'}]);
+        } catch (e) {}
+        if (!newWin || newWin.closed || typeof newWin.closed == 'undefined') {
+            reject([{ message: 'Pop-up is blocked please click allow in the top right corner of browser in address bar!' }]);
         }
     }
 })
--- force-app/main/default/aura/FundingDashboardComponent/FundingDashboardComponentHelper.js
+++ force-app/main/default/aura/FundingDashboardComponent/FundingDashboardComponentHelper.js
@@ -1,101 +1,101 @@
 ({
-    getCalendarMin : function(component){
+    getCalendarMin: function(component) {
         var year = new Date().getFullYear() - 5;
         //var min = year+'-01-01';
         var min = '2010-01-01';
-        component.set("v.calendarMin", min);                  
+        component.set("v.calendarMin", min);
     },
-    
-    getCalendarMax : function(component){
+
+    getCalendarMax: function(component) {
         var year = new Date().getFullYear() + 5;
-        var max = year+'-12-31';
-        component.set("v.calendarMax", max);                
+        var max = year + '-12-31';
+        component.set("v.calendarMax", max);
     },
-    setBUCustomSettings : function(component){
-        return new Promise($A.getCallback(function(resolve, reject){
+    setBUCustomSettings: function(component) {
+        return new Promise($A.getCallback(function(resolve, reject) {
             let action = component.get('c.saveBusinessUnitCustomSettings');
             action.setParams({
-                BusinessUnit : component.get('v.selectedBusinessUnitFilter')
+                BusinessUnit: component.get('v.selectedBusinessUnitFilter')
             });
-            action.setCallback(this,function(response){
+            action.setCallback(this, function(response) {
                 let state = response.getState();
-                if(state === 'SUCCESS'){
+                if (state === 'SUCCESS') {
                     console.log(response.getReturnValue());
-                }else if(state === 'ERROR'){
+                } else if (state === 'ERROR') {
                     reject(response.getError());
                 }
             });
             $A.enqueueAction(action);
         }));
     },
-    
-    setDefaultDates : function(component){
-        return new Promise(function(resolve,reject){
-        let dt = new Date();
-        let customSetting = component.get('v.customSetting');  
-        let defaultEndDate = dt.getFullYear() +'-'+ (dt.getMonth() + 1) +'-' + new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate() + '';
-        let defaultStartDate = dt.getFullYear() +'-'+ (dt.getMonth()) +'-01';
-        try{
+
+    setDefaultDates: function(component) {
+        return new Promise(function(resolve, reject) {
+            let dt = new Date();
+            let customSetting = component.get('v.customSetting');
+            let defaultEndDate = dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + new Date(dt.getFullYear(), dt.getMonth() + 1, 0).getDate() + '';
+            let defaultStartDate = dt.getFullYear() + '-' + (dt.getMonth()) + '-01';
+            try {
                 component.set("v.endDate", customSetting.End_Date__c == null ? defaultEndDate : customSetting.End_Date__c);
-                component.set("v.startDate", customSetting.Start_Date__c == null ? defaultStartDate : customSetting.Start_Date__c); 
-            
-            	resolve(true);
-            }catch(e){
-                reject(new Error('Not defined.'));                
-            }  
+                component.set("v.startDate", customSetting.Start_Date__c == null ? defaultStartDate : customSetting.Start_Date__c);
+
+                resolve(true);
+            } catch (e) {
+                reject(new Error('Not defined.'));
+            }
         });
     },
-    
-    getData : function(component){
-        return new Promise($A.getCallback(function(resolve, reject){
+
+    getData: function(component) {
+        return new Promise($A.getCallback(function(resolve, reject) {
             let action = component.get('c.getData');
             action.setParams({
-                startDate : component.get('v.startDate'),
-                endDate : component.get('v.endDate'),
-                BusinessUnit : component.get("v.selectedBusinessUnitFilter")
+                startDate: component.get('v.startDate'),
+                endDate: component.get('v.endDate'),
+                BusinessUnit: component.get("v.selectedBusinessUnitFilter")
             });
-            action.setCallback(this,function(response){
+            action.setCallback(this, function(response) {
                 let state = response.getState();
-                if(state === 'SUCCESS'){
+                if (state === 'SUCCESS') {
                     console.log('getData returns this result:');
                     console.log(response.getReturnValue());
                     resolve(response.getReturnValue());
-                }else if(state === 'ERROR'){
+                } else if (state === 'ERROR') {
                     reject(response.getError());
                 }
             });
             $A.enqueueAction(action);
         }));
     },
-    getReportCongaURL : function(component){
-        return new Promise($A.getCallback(function(resolve, reject){
+    getReportCongaURL: function(component) {
+        return new Promise($A.getCallback(function(resolve, reject) {
             let action = component.get('c.getDrawdownCongaURLs');
-            action.setCallback(this,function(response){
+            action.setCallback(this, function(response) {
                 let state = response.getState();
-                if(state === 'SUCCESS'){
+                if (state === 'SUCCESS') {
                     console.log('drawdown report view all.');
                     console.log(response.getReturnValue());
                     resolve(response.getReturnValue());
-                }else if(state === 'ERROR'){
+                } else if (state === 'ERROR') {
                     reject(response.getError());
                 }
             });
             $A.enqueueAction(action);
         }));
     },
-    setDateCustomSettings : function(component){
-        return new Promise($A.getCallback(function(resolve, reject){
+    setDateCustomSettings: function(component) {
+        return new Promise($A.getCallback(function(resolve, reject) {
             let action = component.get('c.saveDateCustomSettings');
             action.setParams({
-                startDate : component.get('v.startDate'),
-                endDate : component.get('v.endDate')
+                startDate: component.get('v.startDate'),
+                endDate: component.get('v.endDate')
             });
-            action.setCallback(this,function(response){
+            action.setCallback(this, function(response) {
                 let state = response.getState();
-                if(state === 'SUCCESS'){
+                if (state === 'SUCCESS') {
                     console.log('The start date and end date in custom settings have been updated.');
                     console.log(response.getReturnValue());
-                }else if(state === 'ERROR'){
+                } else if (state === 'ERROR') {
                     console.log('The start date and end date in custom settings could not be updated.');
                     reject(response.getError());
                 }
@@ -103,18 +103,18 @@
             $A.enqueueAction(action);
         }));
     },
-    getCustomSettings : function(component){
+    getCustomSettings: function(component) {
         //get report dates from custom setting
         return new Promise($A.getCallback(
-            function(resolve,reject){
-                let action = component.get('c.getCustomSetting');                
-                action.setCallback(this,function(response){
+            function(resolve, reject) {
+                let action = component.get('c.getCustomSetting');
+                action.setCallback(this, function(response) {
                     let state = response.getState();
-                    if(state === 'SUCCESS'){
+                    if (state === 'SUCCESS') {
                         console.log('1');
                         console.log(response.getReturnValue());
                         resolve(response.getReturnValue());
-                    }else if(state === 'ERROR'){
+                    } else if (state === 'ERROR') {
                         reject(response.getError());
                     }
                 });
@@ -122,15 +122,15 @@
             }
         ));
     },
-    
-    errorsHandler : function(errors){
+
+    errorsHandler: function(errors) {
         if (errors[0] && errors[0].message) {
             console.log('Error message: ' + errors[0].message);
             this.showToast('Error', errors[0].message);
         }
     },
-    
-    showToast : function(title, message,type) {
+
+    showToast: function(title, message, type) {
         var toastEvent = $A.get("e.force:showToast");
         toastEvent.setParams({
             "title": title,
@@ -138,5 +138,43 @@
             "type": type
         });
         toastEvent.fire();
+    },
+
+    getPickListValues: function(component, object, field, attributeId) {
+        var picklistgetter = component.get('c.getPickListValues');
+        picklistgetter.setParams({
+            objectType: object,
+            field: field
+        });
+
+
+        picklistgetter.setCallback(this, function(response) {
+            var opts = [];
+            if (response.getState() == 'SUCCESS') {
+                var allValues = response.getReturnValue();
+                for (var i = 0; i < allValues.length; i++) {
+                    if (allValues[i].includes('===SEPERATOR===')) {
+                        opts.push({
+                            class: "optionClass",
+                            label: allValues[i].split('===SEPERATOR===')[0],
+                            value: allValues[i].split('===SEPERATOR===')[1]
+                        });
+                    } else {
+                        opts.push({
+                            class: "optionClass",
+                            label: allValues[i],
+                            value: allValues[i]
+                        });
+                    }
+                }
+                opts.push({
+                    class: "optionClass",
+                    label: 'Consolidated',
+                    value: 'Consolidated'
+                });
+                component.set('v.' + attributeId, opts);
+            }
+        });
+        $A.enqueueAction(picklistgetter);
     }
 })
